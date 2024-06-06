03bbc2065bc320453954d0632d2091923f9f46a6
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-06-06 11:58:37 -0600

    wired urls

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
     content = models.TextField()
     author = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import ListComments, ListPosts, PostDetails, CommentsDetails
+
+
+urlpatterns = [
+    path("posts/", ListPosts.as_view()),
+    path("posts/<int:pk>", PostDetails.as_view()),
+    path("comments", ListComments.as_view()),
+    path("comments/<int:pk>", CommentsDetails.as_view()),
+]
