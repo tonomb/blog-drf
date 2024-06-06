2dac497911c3aee75e0fe12f47d81adc7406c4c5
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-06-06 11:58:25 -0600

    created generics views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+
+from .serializers import CommentSerialier, PostSerializer
+from .models import Comment, Post
+
+
+class ListPosts(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class ListComments(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerialier
+
+
+class CommentsDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerialier
